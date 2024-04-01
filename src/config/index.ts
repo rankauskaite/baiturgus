f1ce25768ac9021f2d20bef878ce12ee9bf94479
Rugile Rankauskaite <rugile.rank@gmail.com>
2024-04-01 22:27:44 +0300

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,44 @@
+export const PRODUCT_CATEGORIES = [
+    {
+        label: 'E-books',
+        value: 'e-books' as const,
+        href: `/products?category=e-books`,
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Games',
+        value: 'games' as const,
+        href: `/products?category=games`,
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: '3D models',
+        value: '3d-models' as const,
+        href: `/products?category=3d-models`,
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Icons',
+        value: 'icons' as const,
+        href: `/products?category=icons`,
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Music',
+        value: 'music' as const,
+        href: `/products?category=music`,
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Templates',
+        value: 'templates' as const,
+        href: `/products?category=templates`,
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+]
+
+const ar = PRODUCT_CATEGORIES
+
 export const NAVBOARD_CATEGORIES = [
     {
         label: 'Discover',
@@ -5,17 +46,17 @@ export const NAVBOARD_CATEGORIES = [
         featured: [
             {
                 name: 'Sale',
-                href:'#',
+                href: `/products?category=sale`,
                 imageSrc: '/nav/ui-kits/sale.jpg',
             },
             {
                 name: 'New Arrivals',
-                href:'#',
+                href: `/products?category=new-arrivals`,
                 imageSrc: '/nav/ui-kits/NewArrivals.jpg',
             },
             {
                 name: 'Bestsellers',
-                href:'#',
+                href: `/products?category=bestsellers`,
                 imageSrc: '/nav/ui-kits/bestsell.jpg',
             },
         ],
@@ -23,98 +64,12 @@ export const NAVBOARD_CATEGORIES = [
     {
         label: 'Categories',
         value: 'categories' as const,
-        featured: [
-            {
-                name: 'E-books',
-                href:'#',
-                imageSrc: '/nav/icons/picks.jpg',
-            },
-            {
-                name: 'Games',
-                href:'#',
-                imageSrc: '/nav/icons/new.jpg',
-            },
-            {
-                name: 'Music',
-                href:'#',
-                imageSrc: '/nav/icons/bestsellers.jpg',
-            },
-            {
-                name: 'Icons',
-                href:'#',
-                imageSrc: '/nav/icons/picks.jpg',
-            },
-            {
-                name: '3D models',
-                href:'#',
-                imageSrc: '/nav/icons/new.jpg',
-            },
-            {
-                name: 'Templates',
-                href:'#',
-                imageSrc: '/nav/icons/bestsellers.jpg',
-            },
-        ],
-    },
-]
-
-export const PRODUCT_CATEGORIES = [
-    {
-        label: 'E-books',
-        value: 'e-books' as const,
-        // featured: [
-        //     {
-        //         name: 'Sale',
-        //         href:'#',
-        //         imageSrc: '/nav/ui-kits/sale.jpg',
-        //     },
-        //     {
-        //         name: 'New Arrivals',
-        //         href:'#',
-        //         imageSrc: '/nav/ui-kits/NewArrivals.jpg',
-        //     },
-        //     {
-        //         name: 'Bestsellers',
-        //         href:'#',
-        //         imageSrc: '/nav/ui-kits/bestsell.jpg',
-        //     },
-        // ],
-    },
-    {
-        label: 'Games',
-        value: 'games' as const,
-        // featured: [
-        //     {
-        //         name: 'Favorite Icon Pics',
-        //         href:'#',
-        //         imageSrc: '/nav/icons/picks.jpg',
-        //     },
-        //     {
-        //         name: 'New Arrivals',
-        //         href:'#',
-        //         imageSrc: '/nav/icons/new.jpg',
-        //     },
-        //     {
-        //         name: 'Bestselling Icons',
-        //         href:'#',
-        //         imageSrc: '/nav/icons/bestsellers.jpg',
-        //     },
-        // ],
-    },
-    {
-        label: '3D models',
-        value: '3d-models' as const
-    },
-    {
-        label: 'Icons',
-        value: 'icons' as const
-    },
-    {
-        label: 'Music',
-        value: 'music' as const
-    },
-    {
-        label: 'Templates',
-        value: 'templates' as const
+        featured: (
+            PRODUCT_CATEGORIES.map(category => ({
+                name: category.label,
+                href: category.href,
+                imageSrc: category.imageSrc,
+            }))
+        )
     },
 ]
