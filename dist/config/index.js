3f9b2c6c5f6d174f51c1b938ee30922e5ab6a700
Rugile Rankauskaite <rugile.rank@gmail.com>
2024-04-01 22:29:17 +0300

    Update index.js

--- dist/config/index.js
+++ dist/config/index.js
@@ -1,6 +1,45 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PRODUCT_CATEGORIES = exports.NAVBOARD_CATEGORIES = void 0;
+exports.NAVBOARD_CATEGORIES = exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        label: 'E-books',
+        value: 'e-books',
+        href: "/products?category=e-books",
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Games',
+        value: 'games',
+        href: "/products?category=games",
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: '3D models',
+        value: '3d-models',
+        href: "/products?category=3d-models",
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Icons',
+        value: 'icons',
+        href: "/products?category=icons",
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Music',
+        value: 'music',
+        href: "/products?category=music",
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+    {
+        label: 'Templates',
+        value: 'templates',
+        href: "/products?category=templates",
+        imageSrc: '/nav/icons/picks.jpg',
+    },
+];
+var ar = exports.PRODUCT_CATEGORIES;
 exports.NAVBOARD_CATEGORIES = [
     {
         label: 'Discover',
@@ -8,17 +47,17 @@ exports.NAVBOARD_CATEGORIES = [
         featured: [
             {
                 name: 'Sale',
-                href: '#',
+                href: "/products?category=sale",
                 imageSrc: '/nav/ui-kits/sale.jpg',
             },
             {
                 name: 'New Arrivals',
-                href: '#',
+                href: "/products?category=new-arrivals",
                 imageSrc: '/nav/ui-kits/NewArrivals.jpg',
             },
             {
                 name: 'Bestsellers',
-                href: '#',
+                href: "/products?category=bestsellers",
                 imageSrc: '/nav/ui-kits/bestsell.jpg',
             },
         ],
@@ -26,97 +65,10 @@ exports.NAVBOARD_CATEGORIES = [
     {
         label: 'Categories',
         value: 'categories',
-        featured: [
-            {
-                name: 'E-books',
-                href: '#',
-                imageSrc: '/nav/icons/picks.jpg',
-            },
-            {
-                name: 'Games',
-                href: '#',
-                imageSrc: '/nav/icons/new.jpg',
-            },
-            {
-                name: 'Music',
-                href: '#',
-                imageSrc: '/nav/icons/bestsellers.jpg',
-            },
-            {
-                name: 'Icons',
-                href: '#',
-                imageSrc: '/nav/icons/picks.jpg',
-            },
-            {
-                name: '3D models',
-                href: '#',
-                imageSrc: '/nav/icons/new.jpg',
-            },
-            {
-                name: 'Templates',
-                href: '#',
-                imageSrc: '/nav/icons/bestsellers.jpg',
-            },
-        ],
-    },
-];
-exports.PRODUCT_CATEGORIES = [
-    {
-        label: 'E-books',
-        value: 'e-books',
-        // featured: [
-        //     {
-        //         name: 'Sale',
-        //         href:'#',
-        //         imageSrc: '/nav/ui-kits/sale.jpg',
-        //     },
-        //     {
-        //         name: 'New Arrivals',
-        //         href:'#',
-        //         imageSrc: '/nav/ui-kits/NewArrivals.jpg',
-        //     },
-        //     {
-        //         name: 'Bestsellers',
-        //         href:'#',
-        //         imageSrc: '/nav/ui-kits/bestsell.jpg',
-        //     },
-        // ],
-    },
-    {
-        label: 'Games',
-        value: 'games',
-        // featured: [
-        //     {
-        //         name: 'Favorite Icon Pics',
-        //         href:'#',
-        //         imageSrc: '/nav/icons/picks.jpg',
-        //     },
-        //     {
-        //         name: 'New Arrivals',
-        //         href:'#',
-        //         imageSrc: '/nav/icons/new.jpg',
-        //     },
-        //     {
-        //         name: 'Bestselling Icons',
-        //         href:'#',
-        //         imageSrc: '/nav/icons/bestsellers.jpg',
-        //     },
-        // ],
-    },
-    {
-        label: '3D models',
-        value: '3d-models'
-    },
-    {
-        label: 'Icons',
-        value: 'icons'
-    },
-    {
-        label: 'Music',
-        value: 'music'
-    },
-    {
-        label: 'Templates',
-        value: 'templates'
+        featured: (exports.PRODUCT_CATEGORIES.map(function (category) { return ({
+            name: category.label,
+            href: category.href,
+            imageSrc: category.imageSrc,
+        }); }))
     },
 ];
